a233cc9c150f2f0a61d05983f2a6925e6ef38277
Ishank Dubey <ishankdubey@gmail.com>
2021-04-15 23:35:31 +0530

    adding musical animation in all the pages

--- resourses/js/devranjani.js
+++ resourses/js/devranjani.js
@@ -1,35 +1,44 @@
 (function(d){
-	var elements = d.getElementsByClassName('down-arrow');
-	if(elements){
-		for (var i=0;i<elements.length;i++){
-		elements[i].addEventListener('click',doit);	
-		elements[i].addEventListener('keyup',function(e){
-			if (e.keyCode == 13) {
-			doit(e);
-			}
-		});	
-		}
-	}
-	function doit(e){
-		if(!e.target.classList.contains('transform-arrow')){
-			expandfunction(true,e);
-		}else{
-			expandfunction(false,e);
-		}
-	}
-	
-	function expandfunction(expand, e){
-		var display = 'block', ariaVal = true;
-		if(!expand){
-			ariaVal = false;
-			display = 'none';
-			e.target.classList.remove('transform-arrow');
-		}else {
-			e.target.classList.add('transform-arrow');
-		}
-		e.target.parentElement.nextElementSibling.style.display = display;
-		e.target.setAttribute('aria-pressed',ariaVal);
-		e.target.setAttribute('aria-expanded',ariaVal);
-	}
-	
+	var container = d.getElementById('container');
+    
+	var trumpet = document.createElement("span");
+	trumpet.innerHTML = "&#127930;";
+    trumpet.classList.add('trumpet', 'go');
+    
+    var guitar = document.createElement("span");
+    guitar.innerHTML = "&#127928;";
+    guitar.classList.add('guitar', 'go');
+    
+    var keyboard = document.createElement("span");
+    keyboard.innerHTML = '&#127929;';
+    keyboard.classList.add('keyboard', 'go');
+    
+    var note = document.createElement("span");
+    note.innerHTML = '&#127925;';
+    note.classList.add('note', 'go');
+    
+    var drum = document.createElement("span");
+    drum.innerHTML = '&#129345;';
+    drum.classList.add('drum', 'go');
+    
+    var violin = document.createElement("span");
+    violin.innerHTML = '&#127931;';
+    violin.classList.add('violin', 'go');
+    
+    container.appendChild(trumpet);
+    container.appendChild(guitar);
+    container.appendChild(keyboard);
+    container.appendChild(note);
+    container.appendChild(drum);
+    container.appendChild(violin);
+    
+    setTimeout(function(){  
+    	trumpet.parentNode.removeChild(trumpet);
+        guitar.parentNode.removeChild(guitar);
+        keyboard.parentNode.removeChild(keyboard);
+        note.parentNode.removeChild(note);
+        drum.parentNode.removeChild(drum);
+        violin.parentNode.removeChild(violin); 
+    }, 30*1000);
+    
 })(document);

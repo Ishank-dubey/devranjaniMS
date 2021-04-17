21d7217e2183623b140ca36f363fdb17be68509d
Ishank Dubey <ishankdubey@gmail.com>
2021-04-18 00:41:49 +0530

    added various background im ages for the pages done till date

--- resourses/js/devranjani.js
+++ resourses/js/devranjani.js
@@ -62,17 +62,99 @@
     var gallery_images = document.getElementsByClassName('gallery-image')[0];
     if(gallery_images){
     	gallery_images.addEventListener('click', function(){
-    		document.getElementsByClassName('larger-photo-container')[0].style.display = 'block';
+    		popOverOpenClose(true);
     	});
     }
     var menu_icon_slide = document.getElementsByClassName('menu-icon-slide')[0];
     if(menu_icon_slide){
     	menu_icon_slide.addEventListener('click', function(){
-    		popOver();
+    		popOverOpenClose();
     	});
     }
-    function popOver() {
-    	document.getElementsByClassName('larger-photo-container')[0].style.display = 'none';
+    function popOverOpenClose(open) {
+    	if(open){
+    		insertPopOverInDOM('https://scontent.fdel17-1.fna.fbcdn.net/v/t1.6435-9/83341420_2829718553782916_624269102722056192_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=bCiQj_lIEAIAX8F65Ew&_nc_ht=scontent.fdel17-1.fna&oh=634acdf4cfb3f96e23744a4f956b30c3&oe=609EF223', 
+    				'Devi Sarasvati Devi Sarasvati Devi Sarasvati Devi Sarasvati Devi Sarasvati Devi Sarasvati ');
+    	}else {
+    		var larger_photo_container = document.getElementsByClassName('larger-photo-container')[0];
+    		if(larger_photo_container){
+    			larger_photo_container.parentNode.removeChild(larger_photo_container);
+    		}
+    	}
+    	
+    	
+    	
+    }
+    function insertPopOverInDOM (src, text) {
+    	var container = document.getElementById('container');
+    	var lcon = document.createElement("div");  
+    	lcon.classList.add('larger-photo-container');
+    	lcon.setAttribute('tabindex', 0);
+    	
+    	container.insertBefore(lcon, container.childNodes[0]);
+    	
+    	var bundle = document.createElement("div");  
+    	bundle.classList.add('bundle');
+    	lcon.appendChild(bundle);
+    	
+    	var next = document.createElement("button");
+    	next.classList.add('next-slide');
+    	next.innerHTML = '&#10095;';
+    	
+    	
+    	var back = document.createElement("button");
+    	back.classList.add('back-slide');
+    	back.innerHTML = '&#10094;';
+    	
+    	
+    	var p = document.createElement("p");
+    	p.classList.add('img-description-common', 'img-p-top');
+    	bundle.appendChild(p);
+    	
+    	var button = document.createElement('button');
+    	button.classList.add('menu-icon-slide');
+    	p.appendChild(button);
+    	
+    	var span1 = document.createElement('span');
+    	span1.classList.add('devranjani-menu-one-slide', 'rotate');
+    	button.appendChild(span1);
+    	
+    	
+    	var span2 = document.createElement('span');
+    	span2.classList.add('devranjani-menu-three-slide', 'rotate');
+    	button.appendChild(span2);
+    	
+    	bundle.appendChild(back);
+    	bundle.appendChild(next);
+    	
+    	var img = document.createElement('img');
+    	img.classList.add('larger-photo');
+    	img.setAttribute('src', src);
+    	
+    	bundle.appendChild(img);
+    	
+    	var p2 = document.createElement('p');
+    	p2.classList.add('img-description-common', 'img-p-bottom');
+    	p2.textContent = text;
+    	
+    	bundle.appendChild(p2);
+    	button.focus();
+    	
+    	next.addEventListener('click', function() {
+    		animateAndChange('https://scontent.fdel17-1.fna.fbcdn.net/v/t31.18172-8/1598568_588345161253611_1402758850_o.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=ObxI9O1AM1wAX-WO_l_&_nc_ht=scontent.fdel17-1.fna&oh=e279866569c77ec026d6454936574395&oe=60A1839F');
+    	});
+    	button.addEventListener('click', function() {
+    		popOverOpenClose();
+    	});
+    	lcon.addEventListener('keyup', function(e) {
+    		if(e.keyCode === 27) {
+    			popOverOpenClose();
+    		} else if (e.keyCode === 39){
+    			animateAndChange('https://scontent.fdel17-1.fna.fbcdn.net/v/t1.6435-9/83341420_2829718553782916_624269102722056192_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=bCiQj_lIEAIAX8F65Ew&_nc_ht=scontent.fdel17-1.fna&oh=634acdf4cfb3f96e23744a4f956b30c3&oe=609EF223');
+    		} else if(e.keyCode === 37) {
+    			animateAndChange('https://scontent.fdel17-1.fna.fbcdn.net/v/t31.18172-8/1598568_588345161253611_1402758850_o.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=ObxI9O1AM1wAX-WO_l_&_nc_ht=scontent.fdel17-1.fna&oh=e279866569c77ec026d6454936574395&oe=60A1839F');
+    		}
+    	});
     }
     
 })(document);

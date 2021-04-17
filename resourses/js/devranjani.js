ad5e61901d6b9b025d83fba19818a36b2d704dac
Ishank Dubey <ishankdubey@gmail.com>
2021-04-17 21:28:10 +0530

    gallery

--- resourses/js/devranjani.js
+++ resourses/js/devranjani.js
@@ -41,4 +41,38 @@
         violin.parentNode.removeChild(violin); 
     }, 30*1000);
     
+    var next_slide = document.getElementsByClassName('next-slide')[0];
+    var timerId;
+    if(next_slide) {
+    	next_slide.addEventListener('click', function () {
+    		animateAndChange('https://scontent.fdel17-1.fna.fbcdn.net/v/t31.18172-8/1598568_588345161253611_1402758850_o.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=ObxI9O1AM1wAX-WO_l_&_nc_ht=scontent.fdel17-1.fna&oh=e279866569c77ec026d6454936574395&oe=60A1839F');
+    	});
+    }
+    function animateAndChange(src) {
+    	var larger_photo = document.getElementsByClassName('larger-photo')[0];
+		larger_photo.style.opacity = "0.0";
+		if(timerId) {
+			clearTimeout(timerId);
+		}
+		timerId = setTimeout(function(){
+    		larger_photo.style.opacity = "1.0";
+    		larger_photo.setAttribute('src', src);
+    	}, 500);
+    }
+    var gallery_images = document.getElementsByClassName('gallery-image')[0];
+    if(gallery_images){
+    	gallery_images.addEventListener('click', function(){
+    		document.getElementsByClassName('larger-photo-container')[0].style.display = 'block';
+    	});
+    }
+    var menu_icon_slide = document.getElementsByClassName('menu-icon-slide')[0];
+    if(menu_icon_slide){
+    	menu_icon_slide.addEventListener('click', function(){
+    		popOver();
+    	});
+    }
+    function popOver() {
+    	document.getElementsByClassName('larger-photo-container')[0].style.display = 'none';
+    }
+    
 })(document);
